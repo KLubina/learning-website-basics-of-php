ca9eba60f80b928ab5b824713652db0aaf8c1411
Kristian Lubina <kristian.lubina@outlook.com>
2025-02-06 19:19:59 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+How I access the Website
+
+1. Download and install XAMPP if I haven't already. (https://www.apachefriends.org/)
+2. Open the XAMPP Control Panel and start Apache.
+3. Copying the PHP files to the folder C:\xampp\htdocs.
+4. In the browser, I enter http://localhost/FILENAME_IN_HTDOCS
+
+- http://localhost/einfache-php-seite 
+- http://localhost/basics-of-PHP/
---
+++ variables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>PHP Variablen</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <h1>PHP Variablen</h1>
+  <p>Variablen sind ein fundamentaler Bestandteil jeder Programmiersprache. In PHP sind sie besonders flexibel:</p>
+  <ul>
+    <li>Variablen beginnen mit einem Dollarzeichen ($) gefolgt vom Variablennamen.</li>
+    <pre><?php $name = "Max"; echo $name; ?></pre>
+    <li>PHP ist lose typisiert, der Typ einer Variablen muss nicht deklariert werden.</li>
+    <pre><?php $alter = 30; echo $alter; ?></pre>
+  </ul>
+  <p>Zurück zum <a href="index.php">Hauptmenü</a>.</p>
+</body>
+</html>
